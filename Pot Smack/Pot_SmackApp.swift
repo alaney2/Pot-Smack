2691e50bf0e814ac0a04b9c7795ea09a07d46c84
Alan Yao <alanyao.training@gmail.com>
2021-01-08 17:34:17 -0800

    all files

---
+++ Pot Smack/Pot_SmackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pot_SmackApp.swift
+//  Pot Smack
+//
+//  Created by Alan Yao on 1/8/21.
+//
+
+import SwiftUI
+
+@main
+struct Pot_SmackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
